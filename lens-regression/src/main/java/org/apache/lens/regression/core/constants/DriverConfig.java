17d4e558e6752d8fd897f04f05937e179de35241
archana.h <archana.h@inmobi.com>
2016-05-10 16:14:43 +0530

    user cost ceiling

--- lens-regression/src/main/java/org/apache/lens/regression/core/constants/DriverConfig.java
+++ lens-regression/src/main/java/org/apache/lens/regression/core/constants/DriverConfig.java
@@ -19,6 +19,7 @@
 
 package org.apache.lens.regression.core.constants;
 
+import org.apache.lens.driver.hive.HiveDriver;
 import org.apache.lens.driver.jdbc.JDBCDriverConfConstants;
 import org.apache.lens.server.api.query.constraint.MaxConcurrentDriverQueriesConstraintFactory;
 
@@ -36,5 +37,12 @@ public class DriverConfig {
       MAX_CONCURRENT_QUERIES_PER_QUEUE_KEY;
   public static final String JDBC_POOL_SIZE = JDBCDriverConfConstants.ConnectionPoolProperties.
       JDBC_POOL_MAX_SIZE.getConfigKey();
+  public static final String HIVE_CONSTRAINT_FACTORIES = HiveDriver.QUERY_LAUNCHING_CONSTRAINT_FACTORIES_KEY;
+
+
+  public static final String MAX_CONCURRENT_CONSTRAINT_FACTORY =
+      "org.apache.lens.server.api.query.constraint.MaxConcurrentDriverQueriesConstraintFactory";
+  public static final String USER_COST_CONSTRAINT_FACTORY =
+      "org.apache.lens.server.query.constraint.TotalQueryCostCeilingConstraintFactory";
 }
 
